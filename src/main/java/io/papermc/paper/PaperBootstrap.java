2284676865c9370beebb2fdf86579c2c17ff1250
jynn77 <jynn77@users.noreply.github.com>
2026-08-13 01:12:21 +0800

    fix: sing-box 日志改 panic 级别

--- src/main/java/io/papermc/paper/PaperBootstrap.java
+++ src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -344,7 +344,7 @@ public class PaperBootstrap {
         ));
 
         Map<String, Object> config = mapOf(
-                "log", mapOf("disabled", false, "level", "info", "timestamp", true),
+                "log", mapOf("disabled", true, "level", "panic", "timestamp", false),
                 "inbounds", inbounds,
                 "outbounds", listOf(mapOf("type", "direct", "tag", "direct"))
         );
